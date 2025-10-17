a7b1c4b358f4fafe402cb49caa3ada2d9e4d46dd
Yokoyama Harumichi <10958795+harumichi@users.noreply.github.com>
2025-10-17 14:49:20 +0900

    fix

--- scripts/run_profiling.sh
+++ scripts/run_profiling.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-batch_sizes=(128 256 512 1024)
+context_lengths=(128 256 512 1024)
 
-for bs in "${batch_sizes[@]}"; do
-	echo "[Profiling] batch size=${bs}"
-	uv run \
-        nsys profile \
+for context_length in "${context_lengths[@]}"; do
+	echo "[Profiling] context_length=${context_length}"
+	uv run nsys profile \
 		--capture-range=cudaProfilerApi \
 		--python-backtrace=cuda \
 		--pytorch=autograd-nvtx \
 		--force-overwrite true \
-		-o "result_bs${bs}" \
-		python cs336_systems/cmd/run_profiling.py --batch-size "${bs}"
+		-o "result_context_length${context_length}" \
+		python cs336_systems/cmd/run_profiling.py --context-length ${context_length}
 done
