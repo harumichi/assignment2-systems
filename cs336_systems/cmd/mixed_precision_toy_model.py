fa05458ed465e903509f30ebf594266314d6719f
Yokoyama Harumichi <10958795+harumichi@users.noreply.github.com>
2025-10-17 15:45:09 +0900

    mix precision

---
+++ cs336_systems/cmd/mixed_precision_toy_model.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import logging
+
+import cs336_systems
+
+logger = logging.getLogger(__name__)
+
+
+class ToyModel(nn.Module):
+    def __init__(self, in_features: int, out_features: int):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, 10, bias=False)
+        self.ln = nn.LayerNorm(10)
+        self.fc2 = nn.Linear(10, out_features, bias=False)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        logger.info("output linear 1: %s", x.dtype)
+        x = self.ln(x)
+        logger.info("output layernorm: %s", x.dtype)
+        x = self.fc2(x)
+        logger.info("output linear 2: %s", x.dtype)
+        return x
+
+
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+logger.info("Using device: %s", device)
+
+model = ToyModel(in_features=128, out_features=5).to(device)
+
+with torch.autocast(device, dtype=torch.float16):
+    target = torch.randint(0, 5, (32,)).to(device)
+    criterion = nn.CrossEntropyLoss()
+    x = torch.randn(32, 128).to(device)
+    y = model(x)
+    loss = criterion(y, target)
+    loss.backward()
+
+    logger.info("logits: %s", y.dtype)
+    logger.info("loss: %s", loss.dtype)
+
+    for name, param in model.named_parameters():
+        logger.info("param %s: %s", name, param.dtype)
+        if param.grad is not None:
+            logger.info("grad %s: %s", name, param.grad.dtype)
